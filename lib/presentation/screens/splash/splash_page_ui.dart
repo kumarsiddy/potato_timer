8a96abcf9c72de1b18bea04ef59c84f63ddec8bd
kumarsiddy <kumarsiddy@gmail.com>
2023-08-18 14:14:37 +0530

    splash screen changes done

--- lib/presentation/screens/splash/splash_page_ui.dart
+++ lib/presentation/screens/splash/splash_page_ui.dart
@@ -6,14 +6,14 @@ class _SplashUI extends StatelessWidget {
     return ParentWrapperWidget(
       height: SizeConfig.safeAreaScreenHeightWithoutToolbar,
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Center(
-            child: AppImage(
-              imageAsset: ImageAsset.splash,
-              height: 180.h,
-            ),
+          Gap(184.r),
+          AppImage(
+            imageAsset: ImageAsset.splash,
+            height: 180.h,
           ),
           Gap(32.r),
           AppText.displayLarge(StringKey.potatoTimer),
